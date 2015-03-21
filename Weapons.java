b38ebd30d1edbd1c2719c12bc340d48af949930a
AlynTalent <sam12d@my.fsu.edu>
2015-03-21 13:33:18 -0400

    Update Weapons.java

--- Weapons.java
+++ Weapons.java
@@ -9,16 +9,21 @@ public class Weapons {
 
     public Weapons() {
         base_damage = 1;
-        upgrade_damage = 0;
+        upgrade_damage = 1;
         base_cooldown = 1;
-        upgrade_damage = 0;
+        upgrade_cooldown = 1;
         type = 'a';
+        equipped = true;
+        owned = true;
+        price = 0;
     }
 
-    public Weapons(char t) {
+    public Weapons(char t, boolean e, boolean o) {
         setType(t);
-        upgrade_damage = 0;
-        upgrade_cooldown = 0;
+        upgrade_damage = 1;
+        upgrade_cooldown = 1;
+        equipped = e;
+        owned = o;
     }
 
 
@@ -28,22 +33,27 @@ public class Weapons {
         if (type == 'a') {
             base_damage = 1;
             base_cooldown = 1;
+            price = 0;
         }
         else if (type == 'b'){
             base_damage = 2;
             base_cooldown = 2;
+            price = 100;
         }
         else if (type == 'c') {
             base_damage = 5;
             base_cooldown = 5;
+            price = 200;
         }
         else if (type == 'd') {
             base_damage = 8;
             base_cooldown = 8;
+            price = 400;
         }
         else if (type == 'e') {
             base_damage = 10;
             base_cooldown = 10;
+            price = 600;
         }
         else {
             base_damage = 1;
@@ -51,6 +61,13 @@ public class Weapons {
         }
     }
 
+    public void UpgradeDamage(){
+        upgrade_damage = upgrade_damage * 2;
+    }
+
+    public int TotalDamage() {
+        return base_damage * upgrade_damage;
+    }
 
 
     public char getType(){
@@ -81,12 +98,19 @@ public class Weapons {
         equipped = !equipped;
     }
 
+    public void ToggleOwned() { owned = !owned; }
+
+    public Bitmap getImage() { return image; }
 
 
+
+    private Bitmap image;
     private char type;
     private int base_damage;
     private int upgrade_damage;
     private int base_cooldown;
     private int upgrade_cooldown;
+    private int price;
     private boolean equipped;
+    private boolean owned;
 }
