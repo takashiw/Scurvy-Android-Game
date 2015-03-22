a6f1c1fdec99174cb8f9d9e1c3d29ff567168153
Dspritzman <Dspritzman@gmail.com>
2015-03-22 05:53:06 -0400

    Update SeaHorse.java

--- SeaHorse.java
+++ SeaHorse.java
@@ -1,30 +1,36 @@
+package com.example.dspritzman.myapplication;
+
+import java.util.Random;
+
 import java.util.Random;
 
 
 class SeaHorse {
-	private int damageInflicting;
-	private int health;
-	private int worth;
-	private int x;
-	private int y;
-	private int range;
-	private int xSpeed;
-	private boolean dead;
-	private int yDeath;
-	private boolean boss;
-	
-	SeaHorse(){
-		damageInflicting = 1;
-		health = 10;
-		worth = 46;
-		x = 1330;
-		y = 200;
-		range = 20;
-		xSpeed = 5;
-		dead = false;
-		boss = false;
-	}
-	
+    private int damageInflicting;
+    private int health;
+    private int worth;
+    private int x;
+    private int y;
+    private int range;
+    private int xSpeed;
+    private boolean dead;
+    private int yDeath;
+    private boolean boss;
+    private int radius;
+
+    SeaHorse(){
+        damageInflicting = 30;
+        health = 175;
+        worth = 46;
+        x = 200;
+        y = 1400;
+        range = 75;
+        xSpeed = 3;
+        dead = false;
+        boss = false;
+        radius = 50;
+    }
+
 	/*
 	Enemy(int damageInflicting, int health, String name, int worth, int x, int y, int range, int xSpeed){
 		this.damageInflicting = damageInflicting;
@@ -36,80 +42,82 @@ class SeaHorse {
 		this.range = range;
 	}
 	*/
-	//
-	
-	public void setHealth(int health){
-		this.health = health;
-	}
-	
-	public void setWorth(int worth){
-		this.worth = worth;
-	}
-	
-	public void setRange(int range){
-		this.range = range;
-	}
-	
-	public void move(){
-		if(!dead) x += xSpeed;
-		else y -= yDeath;
-	}
-	
-	public void setX(int x){
-		this.x = x;
-	}
-	
-	
-	public int inflictDamage(){
-		if(x < range)	return damageInflicting;
-		return 0;
-	}
-	
-	public void takeDamage(int hitDamage){
-		if(!dead){
-		health = health - hitDamage;
-		if(health <= 0){
-			dead = true; } }
-	}
-		
-	public boolean getDead(){
-		return dead;
-	}
-	
-	public int getHealth(){
-		return health;
-	}
-	
-	public int getWorth(){
-		Random x = new Random();
-		return x.nextInt(10) + worth;
-	}
-	
-	public void setBoss(boolean input) {
-		boss = input;
-	}
-	
-	public boolean getBoss() {
-		return boss;
-	}
-	
-	public int getX() {
-		return x;
-	}
-	
-	public int getY() {
-		return y;
-	}
-	
-	public int getRange() {
-		return range;
-	}
-	
-	public int getxSpeed() {
-		return xSpeed;
-	}
+    //
+
+    public int getRadius()
+    {
+        return radius;
+    }
+
+    public void setHealth(int health){
+        this.health = health;
+    }
+
+    public void setWorth(int worth){
+        this.worth = worth;
+    }
+
+    public void setRange(int range){
+        this.range = range;
+    }
+
+    public int getRange()
+    {
+        return range;
+    }
+
+    public void move(){
+        if(!dead) y -= xSpeed;
+        else x -= yDeath;
+    }
+
+    public void setX(int x){
+        this.x = x;
+    }
+    public int getxSpeed()
+    {
+        return xSpeed;
+    }
+
+
+    public int inflictDamage(){
+        return damageInflicting;
+    }
+
+    public void takeDamage(int hitDamage){
+        if(!dead){
+            health = health - hitDamage;
+            if(health <= 0){
+                dead = true; } }
+    }
+
+    public boolean getDead(){
+        return dead;
+    }
+
+    public int getHealth(){
+        return health;
+    }
+
+    public int getWorth(){
+        Random x = new Random();
+        return x.nextInt(10) + worth;
+    }
+
+    public void setBoss(boolean input) {
+        boss = input;
+    }
+
+    public boolean getBoss() {
+        return boss;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
 
 }
-
-
-
