74391a0cee848f0af222aa85a7cb73bbc0b7db86
AlynTalent <sam12d@my.fsu.edu>
2015-03-22 02:37:43 -0400

    Update Game.java

--- Game.java
+++ Game.java
@@ -133,6 +133,64 @@ public class Game {
         return lManager.seaIterator(i);
     }
 
+    public double Distance(int x1, int y1, int x2, int y2) {
+        double d = Math.sqrt( (x2-x1)*(x2-x1) + (y2-y1)*(y2-y1) );
+        return d;
+    }
+
+    public void Collision_Detect(){
+        for(int i = 0; i < Bullets.size() && Bullets.size() != 0; i++) {
+            for (int f = 0; Distance(Bullets.get(f).getX(), Bullets.get(f).getY(), flyIterator(f).getX(), flyIterator(f).getY()) < flyIterator.getRadius; f++)
+            {
+                if(Distance(Bullets.get(f).getX(), Bullets.get(f).getY(), flyIterator(f).getX(), flyIterator(f).getY()) < flyIterator.getRadius){
+                    flyIterator(f).takeDamage(50);
+                    Bullets.remove(f);
+                    break;
+                }
+            }
+
+            for (int p = 0; Distance(Bullets.get(p).getX(), Bullets.get(p).getY(), puffIterator(p).getX(), puffIterator(p).getY()) < puffIterator.getRadius; p++)
+            {
+                if(Distance(Bullets.get(p).getX(), Bullets.get(p).getY(), puffIterator(p).getX(), puffIterator(p).getY()) < puffIterator.getRadius){
+                    puffIterator(p).takeDamage(50);
+                    Bullets.remove(p);
+                    break;
+                }
+            }
+
+
+            for (int s = 0; Distance(Bullets.get(s).getX(), Bullets.get(s).getY(), seaIterator(s).getX(), seaIterator(s).getY()) < seaIterator.getRadius; s++)
+            {
+                if(Distance(Bullets.get(s).getX(), Bullets.get(s).getY(), seaIterator(s).getX(), seaIterator(s).getY()) < seaIterator.getRadius){
+                    seaIterator(s).takeDamage(50);
+                    Bullets.remove(s);
+                    break;
+                }
+            }
+
+            for (int m = 0; Distance(Bullets.get(m).getX(), Bullets.get(m).getY(), manIterator(m).getX(), manIterator(m).getY()) < manIterator.getRadius; m++)
+            {
+                if(Distance(Bullets.get(m).getX(), Bullets.get(m).getY(), manIterator(m).getX(), manIterator(m).getY()) < manIterator.getRadius){
+                    manIterator(m).takeDamage(50);
+                    Bullets.remove(m);
+                    break;
+                }
+            }
+
+            for (int n = 0; Distance(Bullets.get(n).getX(), Bullets.get(n).getY(),narIterator(n).getX(), narIterator(n).getY()) < narIterator.getRadius; n++)
+            {
+                if(Distance(Bullets.get(n).getX(), Bullets.get(n).getY(), narIterator(n).getX(), narIterator(n).getY()) < narIterator.getRadius){
+                    narIterator(n).takeDamage(50);
+                    Bullets.remove(n);
+                    break;
+                }
+            }
+            
+        }
+    }
+
+
+
     private int ActiveW;
     private int Money;
     private int Health;
